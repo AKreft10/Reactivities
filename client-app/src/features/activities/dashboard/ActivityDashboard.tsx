1a0b2e83018f3ca9230e3f85a6f86cf5e3b1db1f
Kenziexe <1areknetflix@gmail.com>
2023-04-03 23:50:04 +0200

    init commit

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Grid} from 'semantic-ui-react';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
+import ActivityList from './ActivityList';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
+
+
+export default observer (function ActivityDashboard() {
+
+    const {activityStore} = useStore();
+    const {selectedActivity, editMode} = activityStore;
+    return(
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList/>
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && !editMode &&
+                <ActivityDetails/>}
+                {editMode &&
+                <ActivityForm 
+                />}
+            </Grid.Column>
+        </Grid>
+    )
+
+})
